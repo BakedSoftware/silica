9636d229ff7b1a69afe8b756db97ca5c3e94c464
Kayle Gishen <k@bkdsw.com>
2016-08-01 15:14:38 -0400

    Changed show to use scope instead of global cache

--- src/watchers/show.js
+++ src/watchers/show.js
@@ -1,25 +1,28 @@
 export default function Show() {
-  var element, elements, i, isVisible, k, negate, raw, _i, _len, _ref;
-  _ref = Silica._shws;
-  for (k in _ref) {
-    elements = _ref[k];
-    if (Silica._shws.hasOwnProperty(k)) {
-      raw = k;
-      negate = k[0] === '!';
-      if (negate) {
-        k = k.substr(1);
-      }
-      for (i = _i = 0, _len = elements.length; _i < _len; i = ++_i) {
-        element = elements[i];
-        if (!Silica.isInDOM(element)) {
-          continue;
-        }
-        isVisible = Silica._show($(element), k, negate);
-        if (isVisible && element.classList.contains('hidden')) {
-          element.classList.remove('hidden');
-        } else if (!isVisible && !element.classList.contains('hidden')) {
-          element.classList.add('hidden');
-        }
+  var element, elements, i, isVisible, k, negate, raw;
+  raw = this instanceof jQuery ? this[0] : this;
+  elements = raw.querySelectorAll("[data-show]");
+  if(raw.dataset.show){
+    elements.push(raw);
+  }
+  for(var i = elements.length - 1; i >= 0; i--){
+    element = elements[i];
+    if (!Silica.isInDOM(element)) {
+      continue;
+    }
+
+    k = element.dataset.show;
+    negate = k[0] === "!";
+    if (negate) {
+      k = k.substr(1);
+    }
+
+    isVisible = Silica._show(element, k, negate);
+    if (isVisible && element.classList.contains("hidden")) {
+      element.classList.remove("hidden");
+    } else {
+      if (!isVisible && !element.classList.contains("hidden")) {
+        element.classList.add("hidden");
       }
     }
   }

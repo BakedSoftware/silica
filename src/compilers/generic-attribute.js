1e4fae3d161eedd9559820be63dd8cc34ce9f752
Kayle Gishen <k@bkdsw.com>
2016-08-15 14:50:30 -0400

    Allow generic to call functions with params

--- src/compilers/generic-attribute.js
+++ src/compilers/generic-attribute.js
@@ -3,18 +3,33 @@ export default function Href() {
   var nodes = Silica.query(raw, '[data-silica]');
   var node;
   var comps, attribute, valueKey;
+  var params, paramsKeys;
   for (let i = nodes.length - 1; i >= 0; --i)
   {
     node       =  nodes[i];
     comps      =  node.dataset.silica.split("=");
-    if (comps.length !== 2) {
+    if (comps.length !== 2)
+    {
       console.error("Invalid generic binding", node.dataset.silica, "for node", node);
       return
     }
-    attribute  =  comps[0];
-    valueKey   =  comps[1];
 
-    node.setAttribute(attribute, Silica.getValue(node, valueKey));
+    attribute   =  comps[0];
+    valueKey    =  comps[1];
+    paramsKeys  =  valueKey.match("\\((.*)\\)");
+
+    if (paramsKeys !== null)
+    {
+      paramsKeys.shift();
+      params = [];
+      for (let j = 0, length = paramsKeys.length; j < length; j++)
+      {
+        params.push(Silica.getValue(node, paramsKeys[j]));
+      }
+      valueKey = valueKey.substr(0, valueKey.indexOf("("));
+    }
+
+    node.setAttribute(attribute, Silica.getValue(node, valueKey, null, params));
   }
   Silica._capture_links(raw);
 }
--- src/silica.es6
+++ src/silica.es6
@@ -19,7 +19,7 @@ var Silica = {
   _appRoot              :  null,
   interpolationPattern  :  /\{\{(.*?)\}\}/,
   usePushState          :  true,
-  version               :  "0.8.0",
+  version               :  "0.8.1",
 
   // Set the root context
   setContext(contextName)

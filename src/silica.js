c1542a7befc3ca3115a7250c4dbcfcab31c2ca75
Kayle Gishen <k@bkdsw.com>
2017-06-05 20:24:32 -0400

    Fixed versioning

--- src/silica.js
+++ src/silica.js
@@ -25,7 +25,7 @@ window['Silica'] = {
   _clickOutElements     :  new Set(),
   interpolationPattern  :  /\{\{(.*?)\}\}/,
   usePushState          :  true,
-  version               :  "0.16.3",
+  version               :  "0.16.4",
 
   // Set the root context
   setContext(contextName)

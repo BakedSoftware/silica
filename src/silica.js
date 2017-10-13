10400942e1520050e0133729e54800891eb3a79c
jdejesus007 <jdejesus@deliverydudes.com>
2017-10-13 01:31:30 -0400

    missed one

--- src/silica.js
+++ src/silica.js
@@ -29,7 +29,7 @@ window['Silica'] = {
   _clickOutElements     :  new Set(),
   interpolationPattern  :  /\{\{(.*?)\}\}/,
   usePushState          :  true,
-  version               :  "0.21.0",
+  version               :  "0.22.0",
 
   // Set the root context
   setContext(contextName)

d912f8d41fcda8d290ceae670953740106da137b
Kayle Gishen <k@bkdsw.com>
2018-05-14 01:48:34 -0400

    More formatting

--- src/compilers/mousedown.js
+++ src/compilers/mousedown.js
@@ -1,16 +1,15 @@
-goog.module('compilers.mousedown');
+goog.module('compilers.mousedown')
 
-function MouseDown() {
-  var nodes = Silica.query(this, "[data-mousedown]");
-  var node;
-  for (let i = nodes.length - 1; i >= 0; --i)
-  {
-    node = nodes[i];
-    node._rt_live = true;
-    node.onmousedown = function(evt) {
-      Silica._call(this, evt, 'mousedown');
-    };
+function MouseDown () {
+  var nodes = Silica.query(this, '[data-mousedown]')
+  var node
+  for (let i = nodes.length - 1; i >= 0; --i) {
+    node = nodes[i]
+    node._rt_live = true
+    node.onmousedown = function (evt) {
+      Silica._call(this, evt, 'mousedown')
+    }
   }
 }
 
-exports = MouseDown;
+exports = MouseDown

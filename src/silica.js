6d96e4fc2143aff05263c82302defdd8bb168dd9
Kayle Gishen <k@bkdsw.com>
2019-06-17 15:14:51 -0400

    Version bump

--- src/silica.js
+++ src/silica.js
@@ -41,7 +41,7 @@ window['Silica'] = {
     'siO2IncludedUrl', 'src', 'siO2HardClass', 'noStopPropagation', 'noPreventDefault',
     'siO2TemplateId', 'siO2Directive'
   ]),
-  version: '0.60.0-beta8',
+  version: '0.60.0-beta9',
 
   // Set the root context
   setContext (contextName) {
